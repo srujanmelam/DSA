774981c31784d4a59039720a872a50cea77d7392
srujanmelam <srujanmelam@gmail.com>
2022-09-22 01:51:23 +0530

    Revert "added pappa"
    This reverts commit cd4204028e08f5408dd545d05e9809a6fdf26437.

--- Pappa.java
+++
@@ -1,7 +0,0 @@
-public class Pappa {
-    public static void main(String[] args) {
-        System.out.println("Hello Hyper");
-        System.out.println("Hippie");
-        System.out.println("Nibbi");
-    }
-}
